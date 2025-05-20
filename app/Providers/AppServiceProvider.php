d01b6fcd6c10a0304e6b5048f2a89cfd93d1b741
nguyentienvu4897 <nguyentienvu4897@gmail.com>
2025-05-20 23:29:03 +0700

    vpplongnguyen

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -58,7 +58,7 @@ class AppServiceProvider extends ServiceProvider
                     $query->with(['typetwo'])->where('status',1)->orderBy('id','DESC')->select('cate_id','id', 'name','avatar','slug','cate_slug');
                 }
             ])->where('status',1)->orderBy('id','ASC')->get(['id','name','imagehome','avatar','slug','content'])->map(function ($query) {
-                $query->setRelation('product', $query->product->take(8));
+                $query->setRelation('product', $query->product->take(10));
                 return $query;
             });
             $banner = Banner::where(['status'=>1])->get(['id','image','link','title','description', 'type']);
